8de66749cc9e10b8d526e11edf4c369356294cbc
Nikhil Chaudhary <Nikhil.Chaudhary@teppl.in>
2024-06-19 17:16:09 +0530

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from './routes/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
