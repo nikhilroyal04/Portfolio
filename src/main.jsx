16f83582d3af05287824a5a530ad0127290f61ab
Nikhil Chaudhary <Nikhil.Chaudhary@teppl.in>
2024-06-21 09:32:30 +0530

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import routes from './routes/routes';
-
-const basename = import.meta.env.PUBLIC_URL || '/Portfolio/'; // Default to '/Portfolio/'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={routes} />
+    <RouterProvider router={routes} />
   </React.StrictMode>
 );
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import FullLayout from "../layouts/FullLayout";
 import Home from "../components/Home/Home";
 import Not_Found from "../components/Not_Found/Not_Found";
@@ -11,15 +7,16 @@ import Projects from "../components/Projects/Projects";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
-    <>
-      <Route path="/Portfolio" element={<FullLayout />}>
-        <Route path="/Portfolio" element={<Home />} />
-        <Route path="/Portfolio/home" element={<Home />} />
-        <Route path="/Portfolio/projects" element={<Projects />} />
-      </Route>
+    <Route path="/" element={<FullLayout />}>
+      <Route index element={<Home />} />
+      <Route path="home" element={<Home />} />
+      <Route path="projects" element={<Projects />} />
       <Route path="*" element={<Not_Found />} />
-    </>
-  )
+    </Route>
+  ),
+  {
+    basename: '/Portfolio', 
+  }
 );
 
 export default routes;
