f0d54b7b39e8c252826c956fbd2a665aa2503836
NikhilRoyal04 <nikhilchaudhary390@gmail.com>
2024-08-31 13:30:21 +0530

    update

--- src/components/Home/Button.jsx
+++ src/components/Home/Button.jsx
@@ -1,27 +1,47 @@
-import React from "react";
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import React, { useState, useEffect } from "react";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import { IconButton, Tooltip } from "@mui/material";
 
 const Button = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const handleScrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
-    <Tooltip title="Click for Top">
-      <IconButton
-        onClick={handleScrollToTop}
-        sx={{
-          position: 'fixed',
-          bottom: '20px',
-          right: '20px',
-          color: 'black',
-          bgcolor: 'white', 
-        }}
-      >
-        <ArrowUpwardIcon />
-      </IconButton>
-    </Tooltip>
+    showButton && (
+      <Tooltip title="Click to scroll to top">
+        <IconButton
+          onClick={handleScrollToTop}
+          sx={{
+            position: "fixed",
+            bottom: "20px",
+            right: "20px",
+            color: "black",
+            bgcolor: "white",
+          }}
+        >
+          <ArrowUpwardIcon />
+        </IconButton>
+      </Tooltip>
+    )
   );
 };
 
